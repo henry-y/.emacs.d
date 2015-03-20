928929c54ad90bb2852a98cdea06e9f96c34be4a
Henry Yin <henry.yin@nokia.com>
2015-03-20 05:06:43 -0400

    Add git-modes magit as submodules

--- custom/setup-convenience.el
+++ custom/setup-convenience.el
@@ -21,6 +21,13 @@
 (require 'undo-tree)
 (global-undo-tree-mode)
 
+(add-to-list 'load-path "~/.emacs.d/3rd/git-modes")
+(add-to-list 'load-path "~/.emacs.d/3rd/magit")
+(eval-after-load 'info
+  '(progn (info-initialize)
+	  (add-to-list 'Info-directory-list "~/.emacs.d/3rd/magit/")))
+(require 'magit)
+
 (global-set-key (kbd "<f5>") 'magit-status)
 
 (provide 'setup-convenience)
