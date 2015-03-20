4846f860ea49f4254fcafc9257f1867428960b1c
Henry Yin <henry.yin@nokia.com>
2015-03-20 23:42:47 +0800

    1. Clean ui 2. Bump helm

--- init.el
+++ init.el
@@ -24,6 +24,10 @@
 
 (setq inhibit-startup-screen t)
 
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+
 (add-to-list 'load-path "~/.emacs.d/custom/")
 
 (require 'setup-helm)
