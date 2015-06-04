f998fca7e4bb0366410ae1e96448d82ea475b236
Henry Yin <henry.yin@nokia.com>
2015-06-04 13:25:03 +0800

    Configure ropemacs

--- custom/setup-python.el
+++ custom/setup-python.el
@@ -9,4 +9,13 @@
 ;; (setq py-switch-buffers-on-execute-p t)
 ;; (setq py-smart-indentation t)
 
+(autoload 'pymacs-apply "pymacs")
+(autoload 'pymacs-call "pymacs")
+(autoload 'pymacs-eval "pymacs" nil t)
+(autoload 'pymacs-exec "pymacs" nil t)
+(autoload 'pymacs-load "pymacs" nil t)
+(autoload 'pymacs-autoload "pymacs")
+(require 'pymacs)
+(pymacs-load "ropemacs" "rope-")
+
 (provide 'setup-python)
