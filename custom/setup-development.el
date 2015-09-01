b64a42c78dc1a5a460ce47c742dfd48c555f6714
Henry Yin <henry.yin@nokia.com>
2015-09-01 17:55:29 +0800

    Add multi-web-mode as submodule

--- custom/setup-development.el
+++ custom/setup-development.el
@@ -154,6 +154,15 @@
   (hs-minor-mode))
 (add-hook 'nxml-mode-hook 'my-nxml-mode-hook)
 
+(require 'multi-web-mode)
+(setq mweb-default-major-mode 'html-mode)
+(setq mweb-tags 
+	  '((php-mode "<\\?php\\|<\\? \\|<\\?=" "\\?>")
+		(js-mode  "<script[^>]*>" "</script>")
+		(css-mode "<style[^>]*>" "</style>")))
+(setq mweb-filename-extensions '("php" "htm" "html" "ctp" "phtml" "php4" "php5"))
+(multi-web-global-mode 1)
+
 (require 'projectile)
 (projectile-global-mode)
 (require 'helm-projectile)
