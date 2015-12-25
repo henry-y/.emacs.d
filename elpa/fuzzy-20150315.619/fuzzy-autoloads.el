0147d85adc998f9fc3bb74dbb9d53b87a85b6ca2
Henry Yin <henry.yin@nokia.com>
2015-12-25 17:38:40 +0800

    Update elpa packages

--- elpa/fuzzy-20150315.619/fuzzy-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; fuzzy-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("fuzzy.el") (21766 19123 589001 688000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; fuzzy-autoloads.el ends here
--- elpa/parent-mode-20141013.1041/parent-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "parent-mode" "20141013.1041" "get major mode's parent modes" 'nil :url "https://github.com/Fanael/parent-mode")
