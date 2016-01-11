dccac49a1cd517b82df975dd7f9531135ccdbfb3
Henry Yin <henry.yin@nokia.com>
2016-01-11 17:10:42 +0800

    Add elisp-slime-nav as submodule

--- custom/setup-development.el
+++ custom/setup-development.el
@@ -189,4 +189,8 @@
 (require 'puml-mode)
 (add-to-list 'auto-mode-alist '("\\.[pi]uml\\'" . puml-mode))
 
+(require 'elisp-slime-nav)
+(dolist (hook '(emacs-lisp-mode-hook ielm-mode-hook))
+  (add-hook hook 'elisp-slime-nav-mode))
+
 (provide 'setup-development)
