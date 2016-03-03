504a03b055582a60fdf5b2cf01f9c3cd3b475450
Henry Yin <henry.yin@nokia.com>
2016-03-03 15:54:35 +0800

    Update GTD (ongoing)

--- custom/setup-org.el
+++ custom/setup-org.el
@@ -1,50 +1,30 @@
 (require 'org-mouse)
 
-(setq org-agenda-files (quote ("~/gtd/")))
-
 (setq org-todo-keywords
-	  (quote ((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d)")
-			  (sequence "WAITING(w@/!)" "HOLD(h@/!)" "|" "CANCELLED(c@/!)"))))
-
+	  (quote ((sequence "TODO(t)" "MAYBE(m)" "PROJECT(p@)" "ONGOING(o!)" "WAIT(w@)" "|" "DONE(d!)" "CANCELLED(c@)"))))
 (setq org-todo-keyword-faces
-	  (quote (("TODO" :foreground "red" :weight bold)
-			  ("NEXT" :foreground "blue" :weight bold)
-			  ("DONE" :foreground "forest green" :weight bold)
-			  ("WAITING" :foreground "orange" :weight bold)
-			  ("HOLD" :foreground "magenta" :weight bold)
-			  ("CANCELLED" :foreground "forest green" :weight bold))))
-
+	  (quote (("TODO" :foreground "#ff0000" :weight bold)
+			  ("MAYBE" :foreground "#ff4500" :weight bold)
+			  ("PROJECT" :foreground "#0000ff" :weight bold)
+			  ("DOING" :foreground "#b22222" :weight bold)
+			  ("WAIT" :foreground "#ff8c00" :weight bold)
+			  ("DONE" :foreground "#00ff00" :weight bold)
+			  ("CANCELLED" :foreground "#7fff00" :weight bold))))
 (setq org-use-fast-todo-selection t)
 (setq org-treat-S-cursor-todo-selection-as-state-change nil)
+(setq org-log-done 'note)
+(setq org-log-into-drawer t)
 
-(setq org-todo-state-tags-triggers
-      (quote (("CANCELLED" ("CANCELLED" . t))
-              ("WAITING" ("WAITING" . t))
-              ("HOLD" ("HOLD" . t))
-              (done ("WAITING") ("HOLD"))
-              ("TODO" ("WAITING") ("CANCELLED") ("HOLD"))
-              ("NEXT" ("WAITING") ("CANCELLED") ("HOLD"))
-              ("DONE" ("WAITING") ("CANCELLED") ("HOLD")))))
-
+(setq org-agenda-files (quote ("~/gtd/")))
 (setq org-directory "~/gtd/org")
 (setq org-default-notes-file "~/gtd/org/refile.org")
 (setq org-capture-templates
       (quote (("t" "todo" entry (file "~/gtd/org/refile.org")
                "* TODO %?\n%U\n%a\n" :clock-in t :clock-resume t)
               ("r" "respond" entry (file "~/gtd/org/refile.org")
-               "* NEXT Respond to %:from on %:subject\nSCHEDULED: %t\n%U\n%a\n" :clock-in t :clock-resume t :immediate-finish t)
+               "* TODO Respond to %:from on %:subject\nSCHEDULED: %t\n%U\n%a\n" :clock-in t :clock-resume t :immediate-finish t)
               ("n" "note" entry (file "~/gtd/org/refile.org")
-               "* %? :NOTE:\n%U\n%a\n" :clock-in t :clock-resume t)
-              ("j" "Journal" entry (file+datetree "~/gtd/org/diary.org")
-               "* %?\n%U\n" :clock-in t :clock-resume t)
-              ("w" "org-protocol" entry (file "~/gtd/org/refile.org") 
-               "* TODO Review %c\n%U\n" :immediate-finish t)
-              ("m" "Meeting" entry (file "~/gtd/org/refile.org")
-               "* MEETING with %? :MEETING:\n%U" :clock-in t :clock-resume t)
-              ("p" "Phone call" entry (file "~/gtd/org/refile.org")
-               "* PHONE %? :PHONE:\n%U" :clock-in t :clock-resume t)
-              ("h" "Habit" entry (file "~/gtd/org/refile.org")
-               "* NEXT %?\n%U\n%a\nSCHEDULED: %(format-time-string \"%<<%Y-%m-%d %a .+1d/3d>>\")\n:PROPERTIES:\n:STYLE: habit\n:REPEAT_TO_STATE: NEXT\n:END:\n"))))
+               "* %? :NOTE:\n%U\n%a\n" :clock-in t :clock-resume t))))
 
 (setq org-clock-out-remove-zero-time-clocks t)
 
@@ -55,8 +35,12 @@
 (defun bh/verify-refile-target ()
   "Exclude todo keywords with a done state from refile targets"
   (not (member (nth 2 (org-heading-components)) org-done-keywords)))
-
 (setq org-refile-target-verify-function 'bh/verify-refile-target)
+
+(setq org-enforce-todo-dependencies t)
+(setq org-enforce-todo-checkbox-dependencies t)
+(setq org-agenda-dim-blocked-tasks nil)
+(setq org-agenda-compact-blocks t)
 
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-cc" 'org-capture)
@@ -77,5 +61,7 @@
 			 ("+" (:strike-through t)))))
 
 (require 'ox-reveal)
+
+(add-to-list 'auto-mode-alist '("\\.\\(org\\|txt\\)\\'" . org-mode))
 
 (provide 'setup-org)
